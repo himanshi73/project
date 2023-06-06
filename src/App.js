c76a6d57b483471dc30414b25b1e5525dc07d9be
himanshi73 <himsai12345@gmail.com>
2023-06-06 11:03:29 +0530

    data

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter } from "react-router-dom"
 import RoutersScreen from "./view/routers/RoutersScreen"
 import './view/style/style.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
+
 export default function App(){
   return(
     <>
---
+++ src/view/data/Data.js
@@ -0,0 +1,24 @@
+export var toptitle="EDUKATE"
+export var menu=['Home','About','Courses','Pages','Contact']
+export var button=<button>Join Us</button>
+export var banner=[require("../img/logo/background.jpg")]
+export var imgcontent=[<h1>learn from home</h1>,
+                          <h1>Education Courses</h1>]
+export var button=<button>search</button>
+export var about=[require("../img/images/graduation.jpg")]     
+export var content=["First Choice For Online Education Anywhere","Tempor erat elitr at rebum at at clita aliquyam consetetur. Diam dolor diam ipsum et, tempor voluptua sit consetetur sit. Aliquyam diam amet diam et eos sadipscing labore. Clita erat ipsum et lorem et sit, sed stet no labore lorem sit. Sanctus clita duo justo et tempor consetetur takimata eirmod, dolores takimata consetetur invidunt magna dolores aliquyam dolores dolore. Amet erat amet et magna"]      
+export var choose=[require("../img/images/book.jpg")]
+export var content2=["Aliquyam accusam clita nonumy ipsum sit sea clita ipsum clita, ipsum dolores amet voluptua duo dolores et sit ipsum rebum, sadipscing et erat eirmod diam kasd labore clita est. Diam sanctus gubergren sit rebum clita amet."]
+export var cources=["Checkout New Releases Of Our Courses"]
+export var images=[require("../img/images/boy.jpg"),
+                     require("../img/images/laptop.jpg"),
+                     require("../img/images/laptop.jpg"),
+                     require("../img/images/laptop.jpg")
+                    ]
+       
+export var instructor="Meet Our Instructors"
+export var teachers=[require("../img/images/teacher1.jpg"),
+                require("../img/images/teacher2.jpg"),
+                require("../img/images/teacher3.jpg")] 
+export var name=['name1','name2','name3']
+export var studentsay=["What Say Our Students","Dolor est dolores et nonumy sit labore dolores est sed rebum amet, justo duo ipsum sanctus dolore magna rebum sit et. Diam lorem ea sea at. Nonumy et at at sed justo est nonumy tempor. Vero sea ea eirmod, elitr ea amet diam ipsum at amet. Erat sed stet eos ipsum diam"]
